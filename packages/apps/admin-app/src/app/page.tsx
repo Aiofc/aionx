834732ee68da6bdf337c26a99b90cd3926068b4d
7wanaio <1295032384@qq.com>
2024-01-22 08:15:23 +0800

    feat(admin-app): create an admin app project
    Create a system backend based on Nest JS and Tailwind CSS

---
+++ packages/apps/admin-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+  <div className=' text-center text-blue-800'>hello word</div>
+  );
+}
