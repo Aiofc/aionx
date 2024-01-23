4456ee29dc3aa47a182fbeacde0b35d99c88e8ea
7wanaio <1295032384@qq.com>
2024-01-23 08:27:21 +0800

    feat(orm): created prisma-schema-kcms
    n

---
+++ packages/dal/orm/prisma-schema-kcms/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'prisma-schema-kcms',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/packages/dal/orm/prisma-schema-kcms',
+};
